25dafb3b0239c26fb390867732b2624a863bbb66
Damon Chase <chasedamon447@gmail.com>
2025-11-28 07:35:01 -0500

    Use XGBRegressor model to see if performance is better. Use Dell stock.

--- main.py
+++ main.py
@@ -12,16 +12,16 @@
 import pandas as pd
 import yfinance as yf
 import numpy as np
-from ta.momentum import RSIIndicator
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+from xgboost import XGBRegressor
 
 def main():
     
-    # Get Apple's stock data for past month
-    apple = yf.Ticker("AAPL")
-    prices = apple.history(period="5y")
+    # Get Dell's stock data for past month
+    dell = yf.Ticker("DELL")
+    prices = dell.history(period="10y")
 
     # New dataframe with features and label
     df = pd.DataFrame(prices)
@@ -41,16 +41,41 @@ def main():
     # Split train and test data maintaining chronological order
     X = df.drop("NextClose", axis=1)
     y = df["NextClose"]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05, shuffle=False)
 
     # Instantiate and train model
-    model = RandomForestRegressor(n_estimators=300, n_jobs=-1)
+    model = XGBRegressor(
+        n_estimators=500,
+        learning_rate=0.05,
+        max_depth=6,
+        subsample=0.8,
+        colsample_bytree=0.8,
+        objective="reg:squarederror",
+        n_jobs=-1,
+        reg_lambda=1.0
+    )
     model.fit(X_train, y_train)
 
     # Test model
     y_pred = model.predict(X_test)
     rmse = np.sqrt(mean_squared_error(y_test, y_pred))
     print(f"RMSE: {rmse}")
-        
+
+    # Plot predicted vs actual
+    dates = y_test.index.to_numpy()
+    plt.plot(dates, y_pred, label="Prediction")
+    plt.plot(dates, y_test, label="Actual")
+    plt.xlabel("Dates")
+    plt.ylabel("Price in USD")
+    plt.title("Actual vs Predicted Next Day Closing Price of Dell Stock")
+    plt.legend()
+    plt.show()
+
+    # Real Use Case: Predict a next day closing price
+    ts = pd.to_datetime("2025-11-25 00:00:00-05:00")
+    print(f"Actual Closing Price on Wednesday November 26th: {df["Close"].loc[ts]}")
+    data = [[123.089996, 127.120003, 123.050003, 125.919998, 126.601001]]
+    print(f"Predicted Closing Price for Wendesday November 26th: {model.predict(data)}")
+
 if __name__ == "__main__":
     main()
